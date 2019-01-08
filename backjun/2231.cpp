28114546c490d9532deb310050fdf58f02a62cec
thisisgod <rlaud1998@naver.com>
2019-01-08 21:41:01 +0900

    Create 2231.cpp

---
+++ backjun/2231.cpp
@@ -0,0 +1,30 @@
+//분해합 20190108 2231
+#include<iostream>
+using namespace std;
+#define f(i,n) for(int i=1;i<=n;i++)
+
+int main()
+{
+	freopen("input.inp", "r", stdin);
+	freopen("out.out", "w", stdout);
+	
+	int n;
+	cin >> n;
+	int sum;
+
+	f(i, n)
+	{
+		sum = i % 10;
+		for (int j = 10; j <= i; j *= 10)
+		{
+			sum += (i / j) % 10;
+		}
+		sum += i;
+		if (sum == n)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+	cout << "0";
+}
