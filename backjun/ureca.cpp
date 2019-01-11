9e1577c2b100bf7c1eded2cf2e0452f449241b42
thisisgod <rlaud1998@naver.com>
2019-01-11 17:30:32 +0900

    Create ureca.cpp

---
+++ backjun/ureca.cpp
@@ -0,0 +1,36 @@
+//유레카 이론 20190111 10448
+#include<iostream>
+using namespace std;
+#define f(i,k,n) for(int i=k;i<n;i++)
+
+int T[46];
+
+int main()
+{
+	freopen("inp.inp", "r", stdin);
+	freopen("out.out", "w", stdout);
+
+	int t, k, result;
+	cin >> t;
+
+	f(i,1, 45) T[i] = T[i - 1] + i;
+	f(test, 0, t)
+	{
+		cin >> k;
+		result = 0;
+		f(i, 1, 45)
+		{
+			if (T[i] > k)break;
+			f(j, 1, 45)
+			{
+				if (T[i] + T[j] > k)break;
+				f(z, 1, 45)
+				{
+					if (T[i] + T[j] + T[z] > k)break;
+					if (T[i] + T[j] + T[z] == k)result = 1;
+				}
+			}
+		}
+		cout << result << "\n";
+	}
+}
